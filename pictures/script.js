529384d6f06bc073100426b6e35be28a4b84bf85
Your Name <your_email@example.com>
2024-11-04 15:03:00 +0530

    Initial commit

---
+++ pictures/script.js
@@ -0,0 +1,8 @@
+// Hamburger Menu Toggle
+const hamburger_menu = document.querySelector(".hamburger-menu");
+const container = document.querySelector(".container");
+
+hamburger_menu.addEventListener("click", () => {
+  container.classList.toggle("active");
+});
+
